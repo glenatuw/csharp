d8aaf4c9adab8c5c8df6aa766cdcefa002944a50
Glen <glen@example.com>
2021-05-12 12:24:40 -0700

    initial checkin

---
+++ ListView/ListView/SecondWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ListView
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+        }
+
+        private void changeSortOrder(string field)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            ListSortDirection newOrder = ListSortDirection.Ascending;
+
+            if (view.SortDescriptions.Count > 0)
+            {
+                if (view.SortDescriptions[0].Direction == ListSortDirection.Ascending)
+                    newOrder = ListSortDirection.Descending;
+                view.SortDescriptions.RemoveAt(0);
+            }
+            view.SortDescriptions.Add(new SortDescription(field, newOrder));
+        }
+
+        private void nameColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            changeSortOrder("Name");
+        }
+
+        private void passwordColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            changeSortOrder("Password");
+        }
+    }
+}
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            var helloPassword = from user in users where user.Password == "hello" select user;
+            Console.WriteLine("hello passwords:");
+            Console.WriteLine(String.Join("\n", helloPassword));
+
+            users.RemoveAll(user => user.Name.ToLower() == user.Password);
+            Console.WriteLine("list after lower case name removes:");
+            Console.WriteLine(String.Join("\n", users));
+
+            users.Remove(users.First(user => user.Password == "hello"));
+            Console.WriteLine("names and passwords after hello first remove:");
+            users.ForEach(user => Console.WriteLine(user.Name + " " + user.Password));
+
+        }
+    }
+}
